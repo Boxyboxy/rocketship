6fb1107589b506e9df675e928e9998d2a19463ec
boxyboxy <eugenengboxiang@gmail.com>
2023-03-20 15:22:34 +0800

    Added some folder structures and middleware that incoroporates loggers, cors

---
+++ packages/backend/src/middleware/adminAuth.js
@@ -0,0 +1,15 @@
+//not being used now
+
+const axios = require("axios");
+
+function isAdmin(req, res, next) {
+  axios
+    .get(`https://dev-v142ohfthr7u4kgs.au.auth0.com/userinfo`, {
+      headers: { Authorization: `${req.headers.authorization}` },
+    })
+    .then(({ data }) => {
+      req.email = data.email;
+      next();
+    });
+}
+module.exports = isAdmin;
---
+++ packages/backend/src/middleware/auth.js
@@ -0,0 +1,9 @@
+// not being used now
+const { auth } = require("express-oauth2-jwt-bearer");
+
+// Authorization middleware. When used, the Access Token must
+// exist and be verified against the Auth0 JSON Web Key Set.
+module.exports = auth({
+  audience: "http://localhost:3001",
+  issuerBaseURL: "https://dev-v142ohfthr7u4kgs.au.auth0.com",
+});
