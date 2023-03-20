5ee64acf338cadf8b04641b6644f8c13c4cc2011
boxyboxy <eugenengboxiang@gmail.com>
2023-03-20 15:05:02 +0800

    Set up sequelize

--- packages/backend/init.sh
+++ packages/backend/init.sh
@@ -1,1 +1,14 @@
 npm init --y
+npm i dotenv express nodemon sequelize
+npm install --save pg pg-hstore
+$ npm install -D sequelize-cli
+npx sequeize init
+npx sequelize db:create
+
+
+##ubuntu pg admin commands
+sudo service postgresql start
+sudo su postgres
+psql boxybox
+\l
+\c rocketship
