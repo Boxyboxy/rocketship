8ecc792d15cea9a1529fc57dd5b07d200111ff6b
Cljwen <carmenlaujwn@gmail.com>
2023-03-27 02:28:17 +0800

    added loginlogout buttons

--- packages/frontend/src/pages/index.js
+++ packages/frontend/src/pages/index.js
@@ -22,6 +22,9 @@ export default function App() {
         <header>
           <Image src="/logo.svg" alt="Logo" width={100} height={100} />
           <p>This is the welcome page.</p>
+          <a href="/api/auth/login">Login</a>
+          <br />
+          <a href="/api/auth/logout">Logout</a>
         </header>
       </div>
     </>
