64a59f5ebc5c3fde8bec00e19898bf61c4abddc2
Cljwen <carmenlaujwn@gmail.com>
2023-04-02 01:45:37 +0800

    updated image in pp

--- packages/frontend/src/pages/projects/[creatorId]/[projectId]/index.js
+++ packages/frontend/src/pages/projects/[creatorId]/[projectId]/index.js
@@ -105,10 +105,7 @@ export default function ProjectPage() {
             </Grid>
             <Grid container>
               <Grid xs={12} sm={12} md={7} lg={6}>
-                <img
-                  src="https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1420&q=80"
-                  width={500}
-                />
+                <img src={specificProject.coverImage} width={500} />
               </Grid>
               <Grid xs={12} sm={12} md={5} lg={6}>
                 <Typography variant="h6">About the project</Typography>
