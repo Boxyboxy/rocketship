1f6a13340b244cee8aae7b3f36470e883a962a3b
oheirelavv <oheirelav@gmail.com>
2023-04-11 18:17:05 +0800

    updated data display in category page

--- packages/frontend/src/components/categoryPage.js
+++ packages/frontend/src/components/categoryPage.js
@@ -1,35 +1,60 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { categorydata } from "../constants/categorydata";
-import styles from "../styles/categorypage.module.css";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import { BACKEND_URL } from "../constants/categorydata";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { categorydata } from '../constants/categorydata';
+import styles from '../styles/categorypage.module.css';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { BACKEND_URL } from '../constants/categorydata';
+import Link from 'next/link';
+
+const categoryMapping = {
+  fintech: 'Fintech',
+  healthtech: 'Healthtech',
+  socialmedia: 'Social Media',
+  games: 'Games',
+  agritech: 'AgriTech',
+  edutech: 'Edutech',
+  ecommerce: 'Ecommerce',
+  fnb: 'FnB'
+};
 
 export default function CategoryPage({ selectedCategory }) {
-  const [featuredProject, setFeaturedProject] = useState(null);
+  const [featuredProjects, setFeaturedProjects] = useState([]);
+  const [randomProject, setRandomProject] = useState(null);
+  const [filteredProjects, setFilteredProjects] = useState([]);
 
   useEffect(() => {
     const fetchFeaturedProject = async () => {
       try {
+        const mappedCategoryName = categoryMapping[selectedCategory.name] || selectedCategory.name;
         const response = await axios.get(
-          `${BACKEND_URL}projects?categoryName=${selectedCategory.name}`
+          // `${BACKEND_URL}projects?categoryName=${mappedCategoryName}`
+          `http://localhost:8080/projects?categoryName=${mappedCategoryName}`
         );
-        setFeaturedProject(response.data[selectedCategory.id]);
-        // console.log(response.data[selectedCategory.name]);
-        // console.log(selectedCategory.categoryId);
-        console.log(selectedCategory.name);
-        console.log(response.data);
+        setFeaturedProjects(Object.values(response.data));
+        // Generate a random index within the range of the projects array
+        const randomIndex = Math.floor(Math.random() * response.data.length);
+        // Use the random index to select a random project
+        setRandomProject(response.data[randomIndex]);
+
+        // Remove selected random project from list of projects
+        const filteredProjects = featuredProjects.filter(
+          (project) => project.id !== randomProject.id
+        );
+        setFilteredProjects(filteredProjects);
+        console.log(randomProject);
+        console.log(featuredProjects);
+        console.log(filteredProjects);
       } catch (err) {
         console.log(err);
       }
     };
     fetchFeaturedProject();
-  }, [selectedCategory.id]);
+  }, [selectedCategory.name]);
 
   const category = categorydata[selectedCategory.name];
 
@@ -43,102 +68,71 @@ export default function CategoryPage({ selectedCategory }) {
         <span className={styles.line}></span>
       </div>
 
-      {/* featured project */}
-      {featuredProject && (
+      {/* random project */}
+      {randomProject && (
         <div className={styles.title}>
           <div className={styles.headerTitle}>FEATURED</div>
           <div className={styles.featuredContainer}>
-            <img
-              className={styles.featuredImg}
-              src={featuredProject.coverImage}
-              alt="fintech"
-            />
+            <Link
+              className={styles.linkName}
+              href={`/projects/${randomProject.userId}/${randomProject.id}`}
+              passHref>
+              <img className={styles.featuredImg} src={randomProject.coverImage} alt="fintech" />
+            </Link>
 
             <div className={styles.txtContainer}>
-              <div className={styles.featuredHeader}>
-                {featuredProject.name}
-              </div>
-              <p className={styles.featuredTxt}>{featuredProject.details}</p>
-              <p className={styles.featuredTxt}>
-                Created by: {featuredProject.userId}
-              </p>
+              <div className={styles.featuredHeader}>{randomProject.name}</div>
+              <p className={styles.featuredTxt}>{randomProject.details}</p>
+              <p className={styles.featuredTxt}>Created by: {randomProject.userId}</p>
             </div>
           </div>
-
-          {/* more related projects */}
-          <span className={styles.line}></span>
-          <div className={styles.headerTitle}>DISCOVER MORE</div>
         </div>
       )}
 
-      <div className={styles.cardsContainer}>
-        <Card sx={{ maxWidth: 345 }}>
-          <CardMedia
-            component="img"
-            height="140"
-            image="/images/planet-01.png"
-            alt="project1"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              {category.project1}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              A personal finance app that helps users manage their budget, track
-              their expenses, and save money.
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Creator Name</Button>
-            <Typography size="small">$2,456 funded</Typography>
-          </CardActions>
-        </Card>
-
-        <Card sx={{ maxWidth: 345 }}>
-          <CardMedia
-            component="img"
-            height="140"
-            image="/images/planet-02.png"
-            alt="project1"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              {category.project2}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              A payment processing solution that allows businesses to accept
-              payments via mobile devices, online, or in-person.
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Creator Name</Button>
-            <Typography size="small">$1,230 funded</Typography>
-          </CardActions>
-        </Card>
-
-        <Card sx={{ maxWidth: 345 }}>
-          <CardMedia
-            component="img"
-            height="140"
-            image="/images/planet-03.png"
-            alt="project1"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              {category.project3}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              An investment platform that offers personalized investment
-              recommendations based on users' financial goals and risk
-              tolerance.
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Creator Name</Button>
-            <Typography size="small">$512 funded</Typography>
-          </CardActions>
-        </Card>
-      </div>
+      {/* more related projects */}
+      <div></div>
+      {featuredProjects && (
+        <div className={styles.title}>
+          <span className={styles.line}></span>
+          <div className={styles.headerTitle}>DISCOVER MORE</div>
+          <div className={styles.cardsContainer}>
+            {filteredProjects.map((result) => (
+              <Card
+                sx={{
+                  maxWidth: 345,
+                  transition: 'transform 0.2s',
+                  '&:hover': { transform: 'scale(1.05)' }
+                }}
+                key={result.id}>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  width="100%"
+                  image={result.coverImage}
+                  alt={result.name}
+                  sx={{ objectFit: 'cover', maxHeight: '140px' }}
+                />
+                <CardContent sx={{ height: 120, overflow: 'hidden' }}>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {result.name}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {result.summary}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Link
+                    className={styles.linkName}
+                    href={`/projects/${result.userId}/${result.id}`}
+                    passHref>
+                    <Button size="small">View More</Button>
+                  </Link>
+                </CardActions>
+              </Card>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- packages/frontend/src/constants/categorydata.js
+++ packages/frontend/src/constants/categorydata.js
@@ -4,111 +4,55 @@ export const categorydata = {
     description:
       'Fintech (short for financial technology) refers to the use of technology to improve and automate financial services. This includes everything from online banking and payment processing to investing and personal finance management.',
     shoutout:
-      'Look out for personal finance management apps that use AI and machine learning to provide personalized investment advice and portfolio management!',
-    featured: 'Cashwise',
-    featuredImg: '/images/fintech.jpg',
-    featuredTxt:
-      'An AI-powered personal finance assistant that helps users optimize their spending, savings, and investments.',
-    project1: 'CryptoSafe',
-    project2: 'BudgetMate',
-    project3: 'Investify'
+      'Look out for personal finance management apps that use AI and machine learning to provide personalized investment advice and portfolio management!'
   },
   healthtech: {
     title: 'HEALTH TECH PROJECTS',
     description:
       'Health tech refers to the use of technology to improve healthcare services and outcomes. This includes everything from electronic health records and telemedicine to wearable devices and health tracking apps.',
     shoutout:
-      'Explore projects such as wearable devices that monitor and track health indicators in real-time, and use AI to analyze the data to provide insights and recommendations!',
-    featured: 'HealthView ',
-    featuredImg: '/images/healthtech.jpg',
-    featuredTxt:
-      'A mobile app that tracks your health metrics and provides personalized recommendations to improve your wellness.',
-    project1: 'MediConnect',
-    project2: 'Mindful',
-    project3: 'WellFit'
+      'Explore projects such as wearable devices that monitor and track health indicators in real-time, and use AI to analyze the data to provide insights and recommendations!'
   },
   fnb: {
     title: 'F&B PROJECTS',
     description:
       'F&B (Food and Beverages) covers all businesses that produces, distributes, and sells food and drinks. This includes everything from restaurants and cafes to food delivery services and beverage manufacturers.',
     shoutout:
-      'Think of cloud kitchens that leverage data analytics and automation to streamline operations and optimize delivery services. Explore more related projects on this page!',
-    featured: 'PlatePal',
-    featuredImg: '/images/fnb.jpg',
-    featuredTxt:
-      'An AI-powered food ordering system that learns your preferences and suggests dishes based on your taste and dietary restrictions.',
-    project1: 'Bitesight',
-    project2: 'RecipeGenie',
-    project3: 'SmartServe'
+      'Think of cloud kitchens that leverage data analytics and automation to streamline operations and optimize delivery services. Explore more related projects on this page!'
   },
   socialmedia: {
     title: 'SOCIAL MEDIA PROJECTS',
     description:
       'Social media refers to online platforms and tools that allow users to create, share, and interact with content and other users. This includes everything from social networking sites like Facebook and Twitter to video sharing platforms like YouTube and TikTok.',
     shoutout:
-      'Explore out-of-the-box social media platforms that use blockchain technology to provide decentralized, secure, and transparent networks that protect user privacy!',
-    featured: 'FriendFy',
-    featuredImg: '/images/socialmedia.jpg',
-    featuredTxt:
-      'A social media platform that uses machine learning to match users with like-minded people and groups based on their interests and activity.',
-    project1: 'TrendHive',
-    project2: 'LinkUp',
-    project3: 'CircleChat'
+      'Explore out-of-the-box social media platforms that use blockchain technology to provide decentralized, secure, and transparent networks that protect user privacy!'
   },
   games: {
     title: 'GAMES PROJECTS',
     description:
       'Games come in any form of interactive entertainment that involves rules, challenges, and objectives. This includes everything from video games and board games to sports and puzzles.',
     shoutout:
-      'Immerse yourself in virtual reality games that create both immersive and interactive gaming experiences, and incorporate machine learning to provide personalized content and gameplay!',
-    featured: 'GameVision',
-    featuredImg: '/images/games.jpg',
-    featuredTxt:
-      'A computer vision platform that uses machine learning to identify and track player movements in real-time for immersive gaming experiences.',
-    project1: 'CrowdPlay',
-    project2: 'LevelUp',
-    project3: 'Playdate'
+      'Immerse yourself in virtual reality games that create both immersive and interactive gaming experiences, and incorporate machine learning to provide personalized content and gameplay!'
   },
   agritech: {
     title: 'AGRITECH PROJECTS',
     description:
       'Agritech (short for agricultural technology) refers to the use of technology to improve and optimize agricultural practices. This includes everything from precision farming and crop monitoring to agricultural drones and smart irrigation systems.',
     shoutout:
-      'Look out for vertical farms that use indoor, LED lighting to produce high-yield, low-footprint crops that are grown using IoT sensors and AI-powered climate control!',
-    featured: 'CropMind',
-    featuredImg: '/images/agritech.jpg',
-    featuredTxt:
-      'An AI-powered crop management system that uses machine learning to optimize farming practices and increase crop yields.',
-    project1: 'FarmGenius',
-    project2: 'LivestockTracker',
-    project3: 'AgriConnect'
+      'Look out for vertical farms that use indoor, LED lighting to produce high-yield, low-footprint crops that are grown using IoT sensors and AI-powered climate control!'
   },
   edutech: {
     title: 'EDUTECH PROJECTS',
     description:
       'Edutech projects involve leveraging technology to enhance and transform traditional educational experiences, such as classroom learning, online courses, and professional development.',
     shoutout:
-      'Look out for the use of virtual and augmented reality for immersive learning experiences, the adoption of adaptive learning algorithms for personalized education, the implementation of collaborative tools for peer-to-peer learning and group projects, and the integration of gamification and game-based learning for engagement and motivation!',
-    featured: 'CropMind',
-    featuredImg: '/images/agritech.jpg',
-    featuredTxt:
-      'An AI-powered crop management system that uses machine learning to optimize farming practices and increase crop yields.',
-    project1: 'FarmGenius',
-    project2: 'LivestockTracker',
-    project3: 'AgriConnect'
+      'Look out for the use of virtual and augmented reality for immersive learning experiences, the adoption of adaptive learning algorithms for personalized education, the implementation of collaborative tools for peer-to-peer learning and group projects, and the integration of gamification and game-based learning for engagement and motivation!'
   },
   ecommerce: {
     title: 'E-COMMERCE PROJECTS',
     description:
-      'Ecommerce projects involve building digital storefronts and marketplaces that allow customers to purchase products and services online.',
+      'E-Commerce projects involve building digital storefronts and marketplaces that allow customers to purchase products and services online.',
     shoutout:
-      'Look out for the use of AI and machine learning for personalized recommendations, the adoption of mobile-first design principles for seamless browsing on smartphones and tablets and the implementation of innovative payment and fulfillment solutions for fast and efficient order processing!',
-    featured: 'CropMind',
-    featuredImg: '/images/agritech.jpg',
-    featuredTxt:
-      'An AI-powered crop management system that uses machine learning to optimize farming practices and increase crop yields.',
-    project1: 'FarmGenius',
-    project2: 'LivestockTracker',
-    project3: 'AgriConnect'
+      'Look out for the use of AI and machine learning for personalized recommendations, the adoption of mobile-first design principles for seamless browsing on smartphones and tablets and the implementation of innovative payment and fulfillment solutions for fast and efficient order processing!'
   }
 };
