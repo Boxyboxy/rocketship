470ae6a6052a8057ad836162c465fa5ab8942dad
boxyboxy <eugenengboxiang@gmail.com>
2023-04-11 22:14:05 +0800

    contributions now work

--- packages/backend/src/controllers/contributionsController.js
+++ packages/backend/src/controllers/contributionsController.js
@@ -1,12 +1,32 @@
-const { project } = require("../db/models");
+const { project, userSkill, user, skill } = require("../db/models");
 const {
   getAllContributions,
   createContribution,
 } = require("../repositories/contributionsRepository");
 
 module.exports = {
-  async getAllContributions(req, res) {
-    const contributions = await getAllContributions();
+  async getAllContributions(query, res) {
+    const { projectId, userSkillId } = query;
+    const options = {
+      include: [
+        { model: project, where: {} },
+        {
+          model: userSkill,
+          where: {},
+          include: [{ model: user }, { model: skill }],
+        },
+      ],
+    };
+
+    if (projectId) {
+      options.include[0].where.id = projectId;
+    }
+
+    if (userSkillId) {
+      options.include[1].where.id = userSkillId;
+    }
+
+    const contributions = await getAllContributions(options);
     return res.json(contributions);
   },
 
