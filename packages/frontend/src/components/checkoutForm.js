8e27bcce8f68836772ff09834c19ea97f94bfce1
oheirelavv <oheirelav@gmail.com>
2023-04-18 16:25:38 +0800

    added ui interface for checkout form

--- packages/frontend/src/components/checkoutForm.js
+++ packages/frontend/src/components/checkoutForm.js
@@ -1,28 +1,39 @@
-import React from "react";
+import React from 'react';
+import NavBar from './navbar';
+import Footer from './footer';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import LoyaltyIcon from '@mui/icons-material/Loyalty';
+import PaidIcon from '@mui/icons-material/Paid';
 import {
   PaymentElement,
   LinkAuthenticationElement,
   useStripe,
-  useElements,
-} from "@stripe/react-stripe-js";
-import { useState, useEffect } from "react";
-import styles from "../styles/checkoutForm.module.css";
+  useElements
+} from '@stripe/react-stripe-js';
+import { useState, useEffect } from 'react';
+import styles from '../styles/checkoutForm.module.css';
 
-export default function CheckoutForm() {
+export default function CheckoutForm({ incentive }) {
   const stripe = useStripe();
   const elements = useElements();
 
-  const [email, setEmail] = useState("");
+  const [email, setEmail] = useState('');
   const [message, setMessage] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-
+  // console.log(incentive);
   useEffect(() => {
     if (!stripe) {
       return;
     }
 
     const clientSecret = new URLSearchParams(window.location.search).get(
-      "payment_intent_client_secret"
+      'payment_intent_client_secret'
     );
 
     if (!clientSecret) {
@@ -31,17 +42,17 @@ export default function CheckoutForm() {
 
     stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
       switch (paymentIntent.status) {
-        case "succeeded":
-          setMessage("Payment succeeded!");
+        case 'succeeded':
+          setMessage('Payment succeeded!');
           break;
-        case "processing":
-          setMessage("Your payment is processing.");
+        case 'processing':
+          setMessage('Your payment is processing.');
           break;
-        case "requires_payment_method":
-          setMessage("Your payment was not successful, please try again.");
+        case 'requires_payment_method':
+          setMessage('Your payment was not successful, please try again.');
           break;
         default:
-          setMessage("Something went wrong.");
+          setMessage('Something went wrong.');
           break;
       }
     });
@@ -62,8 +73,8 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "http://localhost:3000/success",
-      },
+        return_url: 'http://localhost:3000/success'
+      }
     });
 
     // This point will only be reached if there is an immediate error when
@@ -71,37 +82,76 @@ export default function CheckoutForm() {
     // your `return_url`. For some payment methods like iDEAL, your customer will
     // be redirected to an intermediate site first to authorize the payment, then
     // redirected to the `return_url`.
-    if (error.type === "card_error" || error.type === "validation_error") {
+    if (error.type === 'card_error' || error.type === 'validation_error') {
       setMessage(error.message);
     } else {
-      setMessage("An unexpected error occurred.");
+      setMessage('An unexpected error occurred.');
     }
 
     setIsLoading(false);
   };
 
   const paymentElementOptions = {
-    layout: "tabs",
+    layout: 'tabs'
   };
 
   return (
-    <form id="payment-form" onSubmit={handleSubmit}>
-      {/* <LinkAuthenticationElement
+    <div>
+      <NavBar />
+      <div className={styles.container}>
+        <div className={styles.window}>
+          <div className={styles.orderInfo}>
+            <div class="order-info-content">
+              <h2>Order Summary</h2>
+
+              <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                  <TableHead>
+                    <TableRow
+                      sx={{
+                        backgroundColor: '#F0F0F0',
+                        '& th': {
+                          fontSize: '1rem',
+                          fontWeight: 'bold'
+                        }
+                      }}>
+                      <TableCell>Incentive Type</TableCell>
+                      <TableCell align="right">Amount (SGD)</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                      <TableCell component="th" scope="row">
+                        {incentive === 'equity' ? <PaidIcon /> : <LoyaltyIcon />} {incentive}
+                      </TableCell>
+                      <TableCell align="right">$10</TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <form id="payment-form" onSubmit={handleSubmit}>
+        {/* <LinkAuthenticationElement
         id="link-authentication-element"
         onChange={(e) => setEmail(e.target.value)}
       /> */}
-      <PaymentElement id="payment-element" options={paymentElementOptions} />
-      <button
-        disabled={isLoading || !stripe || !elements}
-        id="submit"
-        className={styles.payButton}
-      >
-        <span id="button-text">
-          {isLoading ? <div className="spinner" id="spinner"></div> : "Pay now"}
-        </span>
-      </button>
-      {/* Show any error or success messages */}
-      {message && <div id="payment-message">{message}</div>}
-    </form>
+        <PaymentElement id="payment-element" options={paymentElementOptions} />
+        <button
+          disabled={isLoading || !stripe || !elements}
+          id="submit"
+          className={styles.payButton}>
+          <span id="button-text">
+            {isLoading ? <div className="spinner" id="spinner"></div> : 'Pay now'}
+          </span>
+        </button>
+        {/* Show any error or success messages */}
+        {message && <div id="payment-message">{message}</div>}
+      </form>
+      <Footer />
+    </div>
   );
 }
