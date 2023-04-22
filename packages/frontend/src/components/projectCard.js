5c8ddbc4c13f5bda97cbe672e95354cc7dd7020d
boxyboxy <eugenengboxiang@gmail.com>
2023-04-23 01:35:28 +0800

    removed button from card components

--- packages/frontend/src/components/projectCard.js
+++ packages/frontend/src/components/projectCard.js
@@ -3,7 +3,6 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import { useEffect, useState } from "react";
 import config from "../config";
 import axios from "axios";
