c03d6955a3c62f6ae363d5f4deb3d31d7bb0ae16
boxyboxy <eugenengboxiang@gmail.com>
2023-04-26 11:00:24 +0800

    Added try catch in all controllers

--- packages/backend/src/controllers/skillsController.js
+++ packages/backend/src/controllers/skillsController.js
@@ -5,27 +5,35 @@ const {
 
 module.exports = {
   async getAllSkills(req, res) {
-    const skills = await getAllSkills();
+    try {
+      const skills = await getAllSkills();
 
-    return res.json(skills);
+      return res.json(skills);
+    } catch (err) {
+      console.error(err);
+    }
   },
   async getSkillById(req, res) {
-    const { id } = req.params;
+    try {
+      const { id } = req.params;
 
-    // +id converts a string to number
-    if (isNaN(id) || +id > Number.MAX_SAFE_INTEGER || +id < 0) {
-      const error = new Error("Skill Id must be a valid number");
-      error.status = 400;
-      throw error;
+      // +id converts a string to number
+      if (isNaN(id) || +id > Number.MAX_SAFE_INTEGER || +id < 0) {
+        const error = new Error("Skill Id must be a valid number");
+        error.status = 400;
+        throw error;
+      }
+
+      const skill = await getSkillById(id);
+      if (!skill) {
+        const error = new Error(`Could not find skill with skill id ${id}`);
+        error.status = 400;
+        throw error;
+      }
+
+      return res.json(skill);
+    } catch (err) {
+      console.error(err);
     }
-
-    const skill = await getSkillById(id);
-    if (!skill) {
-      const error = new Error(`Could not find skill with skill id ${id}`);
-      error.status = 400;
-      throw error;
-    }
-
-    return res.json(skill);
   },
 };
